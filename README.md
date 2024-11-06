d1dfee89986284f1fd834d2cd4deebb1dccb9497
suraj0901 <git@stackblitz.com>
2024-11-06 10:58:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# svelte-native-demo
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/suraj0901/svelte-native-demo)
---
+++ app/components/Home.svelte
@@ -0,0 +1,45 @@
+<page>
+  <actionBar title="NativeFlix" />
+  <!-- Add this 👇 -->
+  <stackLayout height="100%">
+    <listView height="100%" separatorColor="transparent" items="{flicks}">
+      <Template let:item>
+        <gridLayout
+          height="280"
+          borderRadius="10"
+          class="bg-secondary"
+          rows="*, auto, auto"
+          columns="*"
+          margin="5 10"
+          padding="0"
+        >
+          <image row="0" margin="0" stretch="aspectFill" src="{item.image}" />
+          <label
+            row="1"
+            margin="10 10 0 10"
+            fontWeight="700"
+            class="text-primary"
+            fontSize="18"
+            text="{item.title}"
+          />
+          <label
+            row="2"
+            margin="0 10 10 10"
+            class="text-secondary"
+            fontSize="14"
+            textWrap="true"
+            text="{item.description}"
+          />
+        </gridLayout>
+      </Template>
+    </listView>
+  </stackLayout>
+</page>
+
+<script>
+  // Add this 👇
+  import { Template } from 'svelte-native/components'
+  import { FlickService } from '../services/flickService'
+
+  let flicks = FlickService.getInstance().getFlicks()
+</script>
